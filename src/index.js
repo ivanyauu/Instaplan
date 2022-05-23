ddb2074542b4d6e6f016203d5811c3e7ca51a015
Calvin Bird <calvinbird@vpn-172-27-55-138.host.ucla.edu>
2022-05-22 22:37:34 -0700

    Added the navbar and basic code for profile and index

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from './App';
+import Navigation from "./navigation.js";
+import Profile from "./profile.js";
+import Home from "./home.js";
 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </Router>
+    <App />
+  </React.StrictMode>
+
+);
 
