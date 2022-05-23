428b1624e5916f72d875e1f8d9ba1dc093f1ea39
Michael Tao <michaeltao0618@gmail.com>
2022-05-23 00:19:18 -0700

    basic mods

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Event from './event.js';
+
 
 function App() {
   return (
     <div className="app">
-        <Event name='my event'/>
+
     </div>
   )
 }
--- src/home.js
+++ src/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import friendProfile from './friendProfile';
+import Event from './event.js';
+
 
 function home() {
     
   return (
-    <friendProfile></friendProfile>
+    <Event name='new event'/>
   )
 }
 
