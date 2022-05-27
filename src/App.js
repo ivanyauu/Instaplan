3190d60f77394e973c0c802415f035cb8dfa5576
Michael Tao <michaeltao0618@gmail.com>
2022-05-27 00:54:32 -0700

    just some movement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-
+import './index.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navigation from "./navigation.js";
+import Profile from "./profile.js";
+import Home from "./home.js";
 
 function App() {
   return (
     <div className="app">
-
+      <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      </Router>
     </div>
   )
 }
