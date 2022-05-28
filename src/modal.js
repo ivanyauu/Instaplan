827df12ed231e52a56cc2669581c9e5e2a73ac2f
Calvin Bird <calvinbird@Calvins-MBP.lan>
2022-05-28 12:20:49 -0700

    removed the form tag and added the close function when the submit button is pressed

--- src/modal.js
+++ src/modal.js
@@ -97,8 +97,12 @@ const Modal = props => {
   function onSubmit(){
     console.log(endAMPM)  
   }
-  
 
+  function submitEvent(){
+    onSubmit();
+    props.onClose();
+  }
+  
   return(
     <div className="modal" onClick={props.onClose}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
@@ -106,76 +110,76 @@ const Modal = props => {
           <h4 className="modal-title">{props.title}</h4>
         </div>
         <div className="modal-body">
-          <form id="get-input">
-
-          <select name="input-month" id="input-month" onChange={updateEventMonth}>
-                <optgroup label="Months">
-                    <option month="1">January</option>
-                    <option month="2">February</option>
-                    <option month="3">March</option>
-                    <option month="4">April</option>
-                    <option month="5">May</option>
-                    <option month="6">June</option>
-                    <option month="7">July</option>
-                    <option month="8">August</option>
-                    <option month="9">September</option>
-                    <option month="10">October</option>
-                    <option month="11">November</option>
-                    <option month="12">December</option>
-                </optgroup> 
-            </select>
-            <select name="input-day" id="input-day" onChange={updateEventDay}>
-                <optgroup label="Day">
-                    <option day="1">1</option>
-                    <option day="2">2</option>
-                    <option day="3">3</option>
-                    <option day="4">4</option>
-                    <option day="5">5</option>
-                    <option day="6">6</option>
-                    <option day="7">7</option>
-                    <option day="8">8</option>
-                    <option day="9">9</option>
-                    <option day="10">10</option>
-                    <option day="11">11</option>
-                    <option day="12">12</option>
-                    <option day="13">13</option>
-                    <option day="14">14</option>
-                    <option day="15">15</option>
-                    <option day="16">16</option>
-                    <option day="17">17</option>
-                    <option day="18">18</option>
-                    <option day="19">19</option>
-                    <option day="20">20</option>
-                    <option day="21">21</option>
-                    <option day="22">22</option>
-                    <option day="23">23</option>
-                    <option day="24">24</option>
-                    <option day="25">25</option>
-                    <option day="26">26</option>
-                    <option day="27">27</option>
-                    <option day="28">28</option>
-                    <option day="29">29</option>
-                    <option day="30">30</option>
-                    <option day="31">31</option>
-                </optgroup> 
-            </select>
-            <select name="input-year" id="input-year" onChange={updateEventYear}>
-                <optgroup label="Year">
-                    <option year="2022">2022</option>
-                    <option year="2023">2023</option>
-                    <option year="2024">2024</option>
-                    <option year="2025">2025</option>
-                    <option year="2026">2026</option>
-                </optgroup> 
-            </select>
+            <span className="get-date">
+              <select name="input-month" id="input-month" onChange={updateEventMonth}>
+                    <optgroup label="Months">
+                        <option month="1">January</option>
+                        <option month="2">February</option>
+                        <option month="3">March</option>
+                        <option month="4">April</option>
+                        <option month="5">May</option>
+                        <option month="6">June</option>
+                        <option month="7">July</option>
+                        <option month="8">August</option>
+                        <option month="9">September</option>
+                        <option month="10">October</option>
+                        <option month="11">November</option>
+                        <option month="12">December</option>
+                    </optgroup> 
+                </select>
+                <select name="input-day" id="input-day" onChange={updateEventDay}>
+                    <optgroup label="Day">
+                        <option day="1">1</option>
+                        <option day="2">2</option>
+                        <option day="3">3</option>
+                        <option day="4">4</option>
+                        <option day="5">5</option>
+                        <option day="6">6</option>
+                        <option day="7">7</option>
+                        <option day="8">8</option>
+                        <option day="9">9</option>
+                        <option day="10">10</option>
+                        <option day="11">11</option>
+                        <option day="12">12</option>
+                        <option day="13">13</option>
+                        <option day="14">14</option>
+                        <option day="15">15</option>
+                        <option day="16">16</option>
+                        <option day="17">17</option>
+                        <option day="18">18</option>
+                        <option day="19">19</option>
+                        <option day="20">20</option>
+                        <option day="21">21</option>
+                        <option day="22">22</option>
+                        <option day="23">23</option>
+                        <option day="24">24</option>
+                        <option day="25">25</option>
+                        <option day="26">26</option>
+                        <option day="27">27</option>
+                        <option day="28">28</option>
+                        <option day="29">29</option>
+                        <option day="30">30</option>
+                        <option day="31">31</option>
+                    </optgroup> 
+                </select>
+                <select name="input-year" id="input-year" onChange={updateEventYear}>
+                    <optgroup label="Year">
+                        <option year="2022">2022</option>
+                        <option year="2023">2023</option>
+                        <option year="2024">2024</option>
+                        <option year="2025">2025</option>
+                        <option year="2026">2026</option>
+                    </optgroup> 
+                </select>
+              </span>
             <br></br>
 
-            <input type="text" id='event-name' placeholder='Title' onChange={updateEventName}/>
+            <input type="text" className="get-name" id='event-name' placeholder='Title' onChange={updateEventName}/>
             <br></br>
-            <input type="text" id='event-description' placeholder='Description' size="50" onChange={updateEventDescription}/>
+            <input type="text" className="get-description" id='event-description' placeholder='Description' size="50" onChange={updateEventDescription}/>
             <br></br>
             <label>start:
-              <input type="text" pattern="\d*" min="1" max="12" name="start-time-hour" placeholder='00' maxLength="2" size="2" onChange={updateStartHour}/>
+              <input type="text" pattern="\d*" className="get-start-time" name="start-time-hour" placeholder='00' maxLength="2" size="2" onChange={updateStartHour}/>
                   
               <input type="text" pattern="\d*" name="start-time-minute" placeholder='00' maxLength="2" size="2" onChange={updateStartMinute}/>
               <select onChange={updateStartAMPM}>
@@ -199,12 +203,12 @@ const Modal = props => {
 
             <div className="modal-footer">
           <button className="close-button" onClick={props.onClose}>Close</button>
-          <button className="submit-button" onClick={onSubmit}>Add</button>
+          <button className="submit-button" onClick={submitEvent}>Add</button>
           </div>
-          </form>
         </div>
       </div>
     </div>
   )
 }
 export default Modal;
+
