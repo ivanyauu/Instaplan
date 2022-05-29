cfe428d65bcc592cf4be4d4d6797d504b9fd5623
Calvin Bird <calvinbird@Calvins-MBP.lan>
2022-05-29 16:08:33 -0700

    added error check for the hour and minute inputs and addes css file changes

--- src/modal.js
+++ src/modal.js
@@ -9,12 +9,13 @@ const Modal = props => {
   const[eventYear, setEventYear]=useState(null)
   const[eventName, setEventName]=useState(null)
   const[eventDescription, setEventDescription]=useState(null)
-  const[startHour, setStartHour]=useState(null)
-  const[startMinute, setStartMinute]=useState(null)
+  const[startHour, setStartHour]=useState('')
+  const[startMinute, setStartMinute]=useState('')
   const[startAMPM, setStartAMPM]=useState(null)
-  const[endHour, setEndHour]=useState(null)
-  const[endMinute, setEndMinute]=useState(null)
+  const[endHour, setEndHour]=useState('')
+  const[endMinute, setEndMinute]=useState('')
   const[endAMPM, setEndAMPM]=useState(null)
+  const[makePublic, setMakePublic]=useState(null)
 
   if(!props.show){
     return null
@@ -40,9 +41,32 @@ const Modal = props => {
     setEventDescription(val.target.value)
   }
 
-  function updateStartHour(val) {
-    setStartHour(val.target.value)
-  }
+  const startHourCheck = (e) => {
+    const re = new RegExp('^([1-9]|1[0-2])$')
+      if (e.target.value === '' || re.test(e.target.value)) {
+        setStartHour(e.target.value);
+      }
+  };
+
+  const startMinuteCheck = (e) => {
+    const re2 = new RegExp('^([1-5]|[1-5][0-9])$')
+      if (e.target.value === '' || re2.test(e.target.value)) {
+        setStartMinute(e.target.value);
+      }
+  };
+  const endHourCheck = (e) => {
+    const re3 = new RegExp('^([1-9]|1[0-2])$')
+      if (e.target.value === '' || re3.test(e.target.value)) {
+        setEndHour(e.target.value);
+      }
+  };
+  const endMinuteCheck = (e) => {
+    const re4 = new RegExp('^([1-5]|[1-5][0-9])$')
+      if (e.target.value === '' || re4.test(e.target.value)) {
+        setEndMinute(e.target.value);
+      }
+  };
+
 
   function updateStartMinute(val) {
     setStartMinute(val.target.value)
@@ -64,38 +88,23 @@ const Modal = props => {
     setEndAMPM(val.target.value)
   }
 
-  function onSubmit(){
-    console.log(eventMonth)  
+  function updatePublic(val) {
+    setMakePublic(val.target.value)
   }
+
   function onSubmit(){
-    console.log(eventDay)  
-  }
-  function onSubmit(){
-    console.log(eventYear)  
-  }
-  function onSubmit(){
-    console.log(eventName)  
-  }
-  function onSubmit(){
-    console.log(eventDescription)  
-  }
-  function onSubmit(){
-    console.log(startHour)  
-  }
-  function onSubmit(){
-    console.log(startMinute)  
-  }
-  function onSubmit(){
-    console.log(startAMPM)  
-  }
-  function onSubmit(){
-    console.log(endHour)  
-  }
-  function onSubmit(){
-    console.log(endMinute)  
-  }
-  function onSubmit(){
-    console.log(endAMPM)  
+    console.log(eventMonth)
+    console.log(eventDay)
+    console.log(eventYear) 
+    console.log(eventName)
+    console.log(eventDescription)
+    console.log(startHour)
+    console.log(startMinute)
+    console.log(startAMPM)
+    console.log(endHour)
+    console.log(endMinute)
+    console.log(endAMPM)
+    console.log(makePublic) 
   }
 
   function submitEvent(){
@@ -105,13 +114,15 @@ const Modal = props => {
   
   return(
     <div className="modal" onClick={props.onClose}>
+    
       <div className="modal-content" onClick={e => e.stopPropagation()}>
         <div className="modal-header">
           <h4 className="modal-title">{props.title}</h4>
         </div>
         <div className="modal-body">
+          <form onSubmit={submitEvent}>
             <span className="get-date">
-              <select name="input-month" id="input-month" onChange={updateEventMonth}>
+              <select name="input-month" id="input-month" onChange={updateEventMonth} required>
                     <optgroup label="Months">
                         <option month="1">January</option>
                         <option month="2">February</option>
@@ -127,7 +138,7 @@ const Modal = props => {
                         <option month="12">December</option>
                     </optgroup> 
                 </select>
-                <select name="input-day" id="input-day" onChange={updateEventDay}>
+                <select name="input-day" id="input-day" onChange={updateEventDay} required>
                     <optgroup label="Day">
                         <option day="1">1</option>
                         <option day="2">2</option>
@@ -162,7 +173,7 @@ const Modal = props => {
                         <option day="31">31</option>
                     </optgroup> 
                 </select>
-                <select name="input-year" id="input-year" onChange={updateEventYear}>
+                <select name="input-year" id="input-year" onChange={updateEventYear} required>
                     <optgroup label="Year">
                         <option year="2022">2022</option>
                         <option year="2023">2023</option>
@@ -174,14 +185,13 @@ const Modal = props => {
               </span>
             <br></br>
 
-            <input type="text" className="get-name" id='event-name' placeholder='Title' onChange={updateEventName}/>
+            <input type="text" className="get-name" id='event-name' placeholder='Title' onChange={updateEventName} required/>
             <br></br>
-            <input type="text" className="get-description" id='event-description' placeholder='Description' size="50" onChange={updateEventDescription}/>
+            <input type="text" className="get-description" id='event-description' placeholder='Description' size="50" onChange={updateEventDescription} required/>
             <br></br>
             <label>start:
-              <input type="text" pattern="\d*" className="get-start-time" name="start-time-hour" placeholder='00' maxLength="2" size="2" onChange={updateStartHour}/>
-                  
-              <input type="text" pattern="\d*" name="start-time-minute" placeholder='00' maxLength="2" size="2" onChange={updateStartMinute}/>
+              <input value={startHour} type="text" pattern="\d*" className="get-start-time" name="start-time-hour" placeholder='00' maxLength="2" size="2" onChange={startHourCheck} required/>
+              <input value={startMinute} type="text" pattern="\d*" name="start-time-minute" placeholder='00' maxLength="2" size="2" onChange={startMinuteCheck}required/>
               <select onChange={updateStartAMPM}>
                 <option ampm-start="am">AM</option>
                 <option ampm-start="pm">PM</option>
@@ -189,10 +199,10 @@ const Modal = props => {
             </label>
             <br></br>
             <label>end:
-            <span className="end-style">
-              <input type="text" pattern="\d*" name="end-time-hour" placeholder='00' maxLength="2" size="2" onChange={updateEndHour} />
+            <span className="end-style" >
+              <input value={endHour} type="text" pattern="\d*" name="end-time-hour" placeholder='00' maxLength="2" size="2" onChange={endHourCheck} required/>
                   
-              <input type="text" pattern="\d*" name="end-time-minute" placeholder='00' maxLength="2" size="2" onChange={updateEndMinute} />
+              <input value={endMinute} type="text" pattern="\d*" name="end-time-minute" placeholder='00' maxLength="2" size="2" onChange={endMinuteCheck} required/>
             
               <select onChange={updateEndAMPM}>
                 <option ampm-end="am">AM</option>
@@ -200,11 +210,15 @@ const Modal = props => {
               </select>
             </span>
             </label>
-
-            <div className="modal-footer">
-          <button className="close-button" onClick={props.onClose}>Close</button>
-          <button className="submit-button" onClick={submitEvent}>Add</button>
-          </div>
+            <br></br>
+            <label>make public
+            <input type="checkbox" name="public-or-private" onChange={updatePublic}/>
+            </label>
+          <div className="modal-footer">
+            <button className="close-button" onClick={props.onClose}>Close</button>
+            <button className="submit-button" type="submit">Add</button>
+          </div>       
+          </form>
         </div>
       </div>
     </div>
