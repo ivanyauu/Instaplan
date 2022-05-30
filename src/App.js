476c73c36c8e73c0ed9e35678646f279b0fab0f0
Ivan Yau <ivanyauu@Ivans-MacBook-Pro.local>
2022-05-29 17:23:40 -0700

    Implemented Searchbar and Profile Modals

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navigation from "./navigation.js";
 import Profile from "./profile.js";
 import Home from "./home.js";
+import SearchPage from "./searchPage.js";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/searchPage" element={<SearchPage />} />
       </Routes>
       </Router>
     </div>
